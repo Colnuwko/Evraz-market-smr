456081fd45ea53ffb0ef55d58de4ae86c6bfb0e0
ВЛАДИСЛАВ ВЛАДИСЛАВ <Vladislav290403@gmail.com>
2024-07-11 19:57:15 +0400

    Добил футер, доабвил карусель отзывов.

--- evraz-market-smr/src/app/Pages/home-page/home-page.component.ts
+++ evraz-market-smr/src/app/Pages/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { StatusBarComponent } from '../../Shared/Modules/status-bar/status-bar.component';
 import { MainGoodsComponent } from '../../Shared/Modules/main-goods/main-goods.component';
+import {FeedbackComponent} from "../../Shared/Modules/feedback/feedback.component";
 
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [StatusBarComponent, MainGoodsComponent],
+  imports: [StatusBarComponent, MainGoodsComponent, FeedbackComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
---
+++ evraz-market-smr/src/app/Shared/Interfaces/feed.ts
@@ -0,0 +1,8 @@
+export interface Feed {
+      "id": string;
+      "name_user": string;
+      "feed_text": string;
+      "image": string;
+      "start": number;
+      "price": number;
+}
