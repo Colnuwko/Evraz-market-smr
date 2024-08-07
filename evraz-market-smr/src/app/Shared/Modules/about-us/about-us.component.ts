f91918cbde345bc0e0bc18b99a232c22a1d5428a
DanilSukach <danil211-1@yandex.ru>
2024-08-07 10:59:22 +0400

    еще добавил

---
+++ evraz-market-smr/src/app/Shared/Modules/about-us/about-us.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NgStyle} from "@angular/common";
+
+@Component({
+  selector: 'app-about-us',
+  standalone: true,
+  imports: [
+    NgStyle
+  ],
+  templateUrl: './about-us.component.html',
+  styleUrl: './about-us.component.css'
+})
+export class AboutUsComponent {
+
+}
